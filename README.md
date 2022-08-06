7fd9a635d03ae08a4b67bbc74c11cae947e77aa3
FrankLin23 <linzhangsheng23@163.com>
2022-08-06 09:55:57 +0800

    feat:init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# challenge-app
+
+A new Flutter project.
+
+
---
+++ android/app/src/main/kotlin/com/rctech/challenge_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rctech.challenge_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
